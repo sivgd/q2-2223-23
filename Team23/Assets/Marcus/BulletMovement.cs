0fba28f3a88917ee13e52f15f0ec2ba86d1cb987
MUKILTEO\1630465 <1630465@mukilteo.wednet.edu>
2023-01-10 11:29:27 -0800

    Bullet mechanics

---
+++ Team23/Assets/Marcus/BulletMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    float movespeed = 7f;
+
+    Rigidbody2D rb;
+
+    Frogella target;
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent < Rigidbody2D> ();
+        target = GameObject.FindObjectOfType<Frogella>();
+        moveDirection = (target.transform.position - transform.position).normalized * movespeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals ("Frogella"))
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+    }
+}
