9f3439f40a71a15e1d0770ffcf920e9086ea83e7
MUKILTEO\1714038 <1714038@mukilteo.wednet.edu>
2023-01-18 13:24:29 -0800

    This timer stuff sucks. Tomorrow will be my day! -Lizzy

--- Team23/Assets/Lizzy/TimerPracticeScene/TryAgainTimer.cs
+++ Team23/Assets/Lizzy/TimerPracticeScene/TryAgainTimer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class TryAgainTimer : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class TryAgainTimer : MonoBehaviour
 
     public TMP_Text TimerTxt;
 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -33,14 +35,16 @@ public class TryAgainTimer : MonoBehaviour
             }
         }
 
-        void updateTimer(float currentTime)
-        {
-            currentTime += 1;
+        //void DisplayTime(float timeToDisplay)
+        //{
+            //timeToDisplay += 1;
 
-            float minutes = Mathf.FloorToInt(currentTime / 60);
-            float seconds = Mathf.FloorToInt(currentTime % 60);
+            //float minutes = Mathf.FloorToInt(currentTime / 60);
+            //float seconds = Mathf.FloorToInt(currentTime % 60);
 
-            TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
-        }
+            //TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+
+            
+        //}
     }
 }
