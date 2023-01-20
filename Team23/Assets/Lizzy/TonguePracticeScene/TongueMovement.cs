214a5129ba8a378e2870410aa6959bec56be5f43
MUKILTEO\1714038 <1714038@mukilteo.wednet.edu>
2023-01-20 13:26:54 -0800

    Lots of stuff

---
+++ Team23/Assets/Lizzy/TonguePracticeScene/TongueMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+
+       //ransform.position = Vector3.MoveToward(transform.position, target, speed * Time.deltaTime);
+    }
+}
