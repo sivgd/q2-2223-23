73bf36a3c2508c067b0f57de5947dcc2fcfb09e7
MUKILTEO\1714038 <1714038@mukilteo.wednet.edu>
2023-01-24 13:25:13 -0800

    animated tile map is a go

--- Team23/Assets/Lizzy/TonguePracticeScene/ForPoints.cs
+++ Team23/Assets/Lizzy/TonguePracticeScene/ForPoints.cs
@@ -6,7 +6,7 @@ public class ForPoints : MonoBehaviour
 {
     public GameObject obj1;
     public GameObject obj2;
-    public LineRenderer lr;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +17,10 @@ public class ForPoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+        float distance = Vector2.Distance(obj1.transform.position, obj2.transform.position);
         Debug.Log(distance);
 
-        lr.SetPosition(0, obj1.transform.position);
-        lr.SetPosition(1, obj2.transform.position);
+        
 
     }
 }
