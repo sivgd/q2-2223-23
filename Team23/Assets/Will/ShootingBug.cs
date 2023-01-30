908af336cdbca39604be6943cb6f02c39f15b6ba
LAPTOP-PSEKN88V\willu <willudy24@gmail.com>
2023-01-29 18:09:03 -0800

    Started bug shooting mechanic

--- Team23/Assets/Will/ShootingBug.cs
+++ Team23/Assets/Will/ShootingBug.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class ShootingBug : MonoBehaviour
 {
-    public bool bugshot;
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    public bool bugsCollected;
     // Start is called before the first frame update
     void Start()
     {
-        bugshot = false;
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (bugshot == true)
-        {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
+        Vector3 rotation = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if (bugsCollected == true)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                //instantiate(bullet, bulletTransform.position, Quaternion.identity);
+            }
         }
     }
 }
