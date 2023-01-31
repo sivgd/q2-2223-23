bcea28cab8ae1afdddf3291588c4ed22177b8d04
MUKILTEO\1714038 <1714038@mukilteo.wednet.edu>
2023-01-31 13:23:22 -0800

    making the bar move; it'll be done before tomorrow

--- Team23/Assets/Lizzy/PollutionBarPractice/PBARMovement.cs
+++ Team23/Assets/Lizzy/PollutionBarPractice/PBARMovement.cs
@@ -9,8 +9,8 @@ public class PBARMovement : MonoBehaviour
     public int enemies2;
     Animator anim;
     public int cleanLevel;
-    public int cleanLevel1 = 50;
-    public GameObject bar;
+    public int cleanLevel1 = 100;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -25,27 +25,64 @@ public class PBARMovement : MonoBehaviour
         
         enemies1 = enemies.Length;
 
-        enemies2 = enemies1 * 4;
+        enemies2 = enemies1 * 5;
         cleanLevel = cleanLevel1 - enemies2;
 
-        if (cleanLevel > 80)
+        if (cleanLevel > 100)
+        {
+            SceneManager.LoadScene("TitleScreenScene");
+        }
+        else if (cleanLevel > 90)
+        {
+            transform.position = new Vector3(-8.25f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 81);
+        }
+        else if (cleanLevel > 80)
         {
             anim.SetInteger("Clean Level", 81);
+            transform.position = new Vector3(-6.9f, 3.46f, 8.83f);
+        }
+        else if (cleanLevel > 70)
+        {
+            transform.position = new Vector3(-5.2f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 51);
+        }
+        else if (cleanLevel > 60)
+        {
+            transform.position = new Vector3(-4f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 51);
         }
         else if (cleanLevel > 50)
         {
             anim.SetInteger("Clean Level", 51);
+            transform.position = new Vector3(-3.5f, 3.46f, 8.83f);
+        }
+        else if (cleanLevel > 40)
+        {
+            transform.position = new Vector3(-2.6f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 21);
+        }
+        else if (cleanLevel > 30)
+        {
+            transform.position = new Vector3(-1.9f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 21);
         }
         else if (cleanLevel > 20)
         {
             anim.SetInteger("Clean Level", 21);
-            transform.position += new Vector3(0.437f, 0, 0);
+            transform.position = new Vector3(-1.3f, 3.46f, 8.83f);
+        }
+        else if (cleanLevel > 10)
+        {
+            transform.position = new Vector3(-0.5f, 3.46f, 8.83f);
+            anim.SetInteger("Clean Level", 19);
         }
         else if (cleanLevel > 0)
         {
             anim.SetInteger("Clean Level", 19);
+            transform.position = new Vector3(0f, 3.46f, 8.83f);
         }
-        else if (cleanLevel == 0)
+        else if (cleanLevel <= 0)
         {
             SceneManager.LoadScene("TitleScreenScene");
         }
