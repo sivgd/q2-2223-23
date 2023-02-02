d5341e8d6d255f8e1081b96c85b2f4535f2d0a91
MUKILTEO\1714036 <1714036@mukilteo.wednet.edu>
2023-02-02 12:30:15 -0800

    Finished water color changes

---
+++ Team23/Assets/Will/WaterCleanerColorChange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaterCleanerColorChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TilemapRenderer rend;
+    public GameObject polution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.gameObject.GetComponent<TilemapRenderer>();
+        rend.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int p = polution.GetComponent<NewPBAR>().cleanLevel;
+
+        if (p < 80 && p > 50)
+        {
+            rend.enabled = true;
+        }
+        if (p <= 50)
+        {
+            rend.enabled = false;
+        }
+    }
+}
+
---
+++ Team23/Assets/Will/WaterCleanestColorChange.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaterCleanestColorChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TilemapRenderer rend;
+    public GameObject polution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.gameObject.GetComponent<TilemapRenderer>();
+        rend.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int p = polution.GetComponent<NewPBAR>().cleanLevel;
+
+        if (p <= 100 && p >= 80)
+        {
+            rend.enabled = true;
+        }
+        if (p < 80)
+        {
+            rend.enabled = false;
+        }
+    }
+}
+
